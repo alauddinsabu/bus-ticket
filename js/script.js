5e20828e793f307235fe988bff868e2d85bd866c
alauddinsabu <alauddinmu76@Gmail.com>
2024-02-20 00:38:43 +0600

    assign edit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,59 @@
-const 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let availableSeats = 40;
+//let availableSeats = 40;
+//    let bookedSeats = 0;
+//
+//    function toggleSeat(seat) {
+//        if (!seat.classList.contains('bg-blue-500')) {
+//            seat.classList.toggle('bg-blue-500');
+//            seat.classList.toggle('text-white');
+//            availableSeats--;
+//            bookedSeats++;
+//            document.getElementById('available-seats').innerText = availableSeats;
+//            const bookedSeatsTable = document.querySelector('#booked-seats tbody');
+//            const newRow = bookedSeatsTable.insertRow(-1);
+//            const cell = newRow.insertCell(0);
+//            cell.textContent = seat.textContent;
+//
+//            if (bookedSeats >= 4) {
+//                const seats = document.getElementsByClassName('seat');
+//                for (let i = 0; i < seats.length; i++) {
+//                    if (!seats[i].classList.contains('bg-blue-500')) {
+//                        seats[i].classList.add('opacity-50');
+//                        seats[i].removeAttribute('onclick');
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//    function hideElementById(elementId){
+//        const elements = document.getElementById(elementId);
+//        elements.classList.add('hidden');
+//    }
+//
+//    function showElementById(elementId){
+//        const elements = document.getElementById(elementId);
+//        elements.classList.remove('hidden'); 
+//    }
+//
+//    function next(){
+//        hideElementById('bus-seat');
+//        showElementById('successPage');
+//    }
+
+
+        
 
